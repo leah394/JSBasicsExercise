7256c786b1f8a9d11786e1082487e09371ec9cde
Maleah Walden <maleah13walden@gmail.com>
2020-10-13 09:55:10 -0500

    Completed Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,9 @@
+console.log(28);
+// Numbers, Strings, Booleans, Null and Undefined
+// +, -, *, /, %
+console.log(7%4);
+console.log(7%-4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Spider-"+"man");
+console.log("MaLeah".length + "Walden".length);
+console.log("Avengers: Endgame"[16]);
